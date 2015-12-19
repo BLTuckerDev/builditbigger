41ac17db96ac020c60c6ef47ba80dff4c5119adf
Brett <bretttucker@gmail.com>
2015-12-19 15:43:08 -0700

    Reformat jokes
    My windows machine cried about unmappable characters when I tried to
    build on it so this should fix that.

--- jokeslib/src/main/java/dev/bltucker/jokes/JokeProvider.java
+++ jokeslib/src/main/java/dev/bltucker/jokes/JokeProvider.java
@@ -11,24 +11,16 @@ public final class JokeProvider {
     private static final List<String> jokes =
             new ArrayList<>(Arrays.asList(
                     "There are 10 kinds of people in the world: Those that know binary & those that don't",
-                    "Two bytes meet. The first byte asks, “Are you ill?”\n" +
-                            "\n" +
-                            "The second byte replies, “No, just feeling a bit off.”",
-                            "Q: how many programmers does it take to change a light bulb?\n" +
-                            "\n" +
-                            "A: none, that's a hardware problem",
+                    "Two bytes meet. The first byte asks, “Are you ill?”\n\n" + "The second byte replies, “No, just feeling a bit off.”",
+                    "Q: how many programmers does it take to change a light bulb?\n\n" + "A: none, that's a hardware problem",
                     "Keyboard not found ... press F1 to continue",
-                            "Q: \"Whats the object-oriented way to become wealthy?\"\n" +
-                            "\n" +
-                            "A: Inheritance",
-                            "Old C programmers don't die, they're just cast into void.",
+                    "Q: \"Whats the object-oriented way to become wealthy?\"\n\n" + "A: Inheritance",
+                    "Old C programmers don't die, they're just cast into void.",
                     "A SQL query goes into a bar, walks up to two tables and asks, \"Can I join you?\"",
-                    "Q: Why don't jokes work in octal?\n" +
-                            "\n" +
-                            "A: Because 7 10 11.",
+                    "Q: Why don't jokes work in octal?\n\n" + "A: Because 7 10 11.",
                     "If Java is the answer, it must have been a really verbose question.",
                     "When your hammer is C++, everything begins to look like a thumb."
-                    ));
+            ));
 
 
     private static final Random random = new Random(System.currentTimeMillis());
